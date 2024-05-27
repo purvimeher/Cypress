83a9d0a4fb1ae98145d0e56b9c65c1becf41c4fa
meherm <meherpurvi2815@gmail.com>
2024-05-27 18:46:32 +0100

    added api sample test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'https://aqueous-brook-60480.herokuapp.com/',
+    // specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    // supportFile: 'cypress/support/index.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
